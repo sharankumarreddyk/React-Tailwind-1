6f01ed1920a8f3e51621a09bbde7550990eab7a5
sharankumarreddyk <167206944+sharankumarreddyk@users.noreply.github.com>
2024-10-28 14:18:10 +0530

    change20

--- src/components/MachineComponents/Machineslider.jsx
+++ src/components/MachineComponents/Machineslider.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import ImageBox from "./ImageBox";
 import Dropdown from "../OrderComponents/Dropdown";
 import i1 from "../../assets/i1.png";
@@ -6,7 +6,8 @@ import i2 from "../../assets/i2.png";
 
 const MachineSlider = ({ selectedMachineIndex, setSelectedMachineIndex }) => {
   const imageContainerRef = useRef(null);
-  
+  const [visibleServiceBox, setVisibleServiceBox] = useState(null);
+
   const machines = [
     {
       title: "Venturi Dryer Alarm",
@@ -26,7 +27,7 @@ const MachineSlider = ({ selectedMachineIndex, setSelectedMachineIndex }) => {
       borderColor: "border-[#FF3334]",
       textColor: "text-[#FF3334]",
     },
-   
+    // Subsequent slides with same styles as the first slide
     {
       title: "Venturi Dryer Alarm",
       branch: "KLM Branch",
@@ -90,24 +91,46 @@ const MachineSlider = ({ selectedMachineIndex, setSelectedMachineIndex }) => {
     setSelectedMachineIndex(index); // Update the index in the parent component
   };
 
+  const toggleServiceBox = (part) => {
+    setVisibleServiceBox(visibleServiceBox === part ? null : part);
+  };
+
   return (
     <div className="bg-white  p-4 ">
       <div className="sm:flex justify-between items-center">
-        <h2 className="text-[#0F1C40] my-3 text-2xl sm:text-3xl md:text-5xl font-bold">Machines</h2>
+        <h2 className="text-[#0F1C40] my-3 text-2xl sm:text-3xl md:text-5xl font-bold">
+          Machines
+        </h2>
         <div className="flex space-x-2 sm:space-x-3 mb-3">
-          <Dropdown id={5} buttonText="Machine Filter" options={["Product Pump", "Machine 2"]} />
-          <Dropdown id={6} buttonText="Department Name" options={["Department 1", "Department 2"]} />
+          <Dropdown
+            id={5}
+            buttonText="Machine Filter"
+            options={["Product Pump", "Machine 2"]}
+          />
+          <Dropdown
+            id={6}
+            buttonText="Department Name"
+            options={["Department 1", "Department 2"]}
+          />
         </div>
       </div>
 
       <div className="relative">
         <div className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 sm:bg-white sm:py-40">
-          <button onClick={() => scroll("left")} className="justify-center font-bold text-center p-2 sm:p-3 px-2 sm:px-4 lg:px-2 rounded-md text-xs sm:text-sm lg:text-lg">
-            <span className="material-symbols-outlined text-[#0F1C40] text-3xl sm:text-5xl">arrow_circle_left</span>
+          <button
+            onClick={() => scroll("left")}
+            className="justify-center font-bold text-center p-2 sm:p-3 px-2 sm:px-4 lg:px-2 rounded-md text-xs sm:text-sm lg:text-lg"
+          >
+            <span className="material-symbols-outlined text-[#0F1C40] text-3xl sm:text-5xl">
+              arrow_circle_left
+            </span>
           </button>
         </div>
 
-        <div ref={imageContainerRef} className="overflow-x-auto bg-white border border-white py-8 sm:py-12 px-4 mr-16 hide-scrollbar sm:rounded-md">
+        <div
+          ref={imageContainerRef}
+          className="overflow-x-auto bg-white border border-white py-8 sm:py-12 px-4 mr-16 hide-scrollbar sm:rounded-md"
+        >
           <div className="flex space-x-4 sm:space-x-6 pl-2 pr-8 sm:pl-20 sm:pr-20">
             {machines.map((machine, index) => (
               <div
@@ -122,14 +145,65 @@ const MachineSlider = ({ selectedMachineIndex, setSelectedMachineIndex }) => {
                   time={machine.time}
                   imageSrc={machine.imageSrc}
                 />
+                {index === 1 && (
+                  <div className="absolute top-20 sm:top-28 right-20 sm:right-32 group">
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        toggleServiceBox("part1");
+                      }}
+                      className="bg-red-600 text-white rounded-full w-4 sm:w-5 px-1 justify-center"
+                    >
+                      X
+                    </button>
+                    {visibleServiceBox === "part1" && (
+                      <div className="absolute mt-5 z-20 bg-white border border-red-500 text-red-500 rounded-md p-2 shadow-lg">
+                        <div className="flex justify-between text-red-500 mt-2 font-bold text-md">
+                          Part 1 <p>Damage</p>
+                        </div>
+                        <div className="flex">
+                          <p className="text-sm">
+                            Temperature: <br />
+                            <span className="font-bold ml-2 text-lg">
+                              210°C
+                            </span>
+                          </p>
+                        </div>
+                        <div className="flex items-center">
+                          <p className="ml-2">
+                            Param 7: <br />
+                            <span className="font-bold text-lg">300 Unit</span>
+                          </p>
+                        </div>
+                        <p className="text-xs text-red-500">
+                          Between 100 and 200 Unit
+                        </p>
+
+                        <div className="flex mt-3 mx-3 space-x-2">
+                          <button className="border text-xs px-2 py-1 rounded-md bg-[#FF3334] text-white">
+                            Service
+                          </button>
+                          <button className="border text-xs px-2 py-1 rounded-md bg-white text-[#FF3334]">
+                            Order
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                )}
               </div>
             ))}
           </div>
         </div>
 
         <div className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 sm:bg-white sm:py-40">
-          <button onClick={() => scroll("right")} className="justify-center font-bold text-center p-2 sm:p-3 px-2 sm:px-4 lg:px-2 rounded-md text-xs sm:text-sm lg:text-lg">
-            <span className="material-symbols-outlined text-[#0F1C40] text-3xl sm:text-5xl">arrow_circle_right</span>
+          <button
+            onClick={() => scroll("right")}
+            className="justify-center font-bold text-center p-2 sm:p-3 px-2 sm:px-4 lg:px-2 rounded-md text-xs sm:text-sm lg:text-lg"
+          >
+            <span className="material-symbols-outlined text-[#0F1C40] text-3xl sm:text-5xl">
+              arrow_circle_right
+            </span>
           </button>
         </div>
       </div>
